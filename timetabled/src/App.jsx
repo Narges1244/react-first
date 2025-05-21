c1cae3a0c4009dbf4a83f9013a769b14f5298931
Narges Salari <narges.sa1359@gmail.com>
2025-05-20 20:29:53 -0400

    Create the Calendar Component

--- timetabled/src/App.jsx
+++ timetabled/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import Calendar from './components/Calendar';
 const App = ()=>{
   return(
     <div className = "App">
       <h1>Personal Productivity Planner🗂️</h1> 
       <h2>A calendar for tracking daily to-do items and one-hour work blocks.</h2>
+      <Calendar />
 
     </div>
   )
---
+++ timetabled/src/components/Calendar.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+const Calendar = () => {
+    return(
+        <div className = "calendar">
+            "Testing calendar component"
+        </div>
+    )
+}
+export default Calendar
