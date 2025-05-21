c88cf6620e397995fe7e7b314b2e39e009008c3a
Narges Salari <narges.sa1359@gmail.com>
2025-05-20 20:16:20 -0400

    Update the Starter Code for Root File App.jsx

--- timetabled/src/App.jsx
+++ timetabled/src/App.jsx
@@ -2,6 +2,8 @@ import './App.css';
 const App = ()=>{
   return(
     <div className = "App">
+      <h1>Personal Productivity Planner🗂️</h1> 
+      <h2>A calendar for tracking daily to-do items and one-hour work blocks.</h2>
 
     </div>
   )
